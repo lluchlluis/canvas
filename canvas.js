0f937d4a74d8f08dcb936eee2e9f425a23c056c8
Lluis <lluchlluis@gmail.com>
2018-08-07 17:05:39 +0200

    last1

--- canvas.js
+++ canvas.js
@@ -37,11 +37,11 @@ function draw() {
 
   //Límites
   if (ball.y + ball.vy > canvas.height ||
-      ball.y + ball.vy < 0) {
+    ball.y + ball.vy < 0) {
     ball.vy = -ball.vy;
   }
   if (ball.x + ball.vx > canvas.width ||
-      ball.x + ball.vx < 0) {
+    ball.x + ball.vx < 0) {
     ball.vx = -ball.vx;
   }
   //Activar animación
